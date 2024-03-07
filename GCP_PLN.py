2798780f1aff1e802a43610928fd4159f56658dd
Jacek <mrozowsky.jacek@gmail.com>
2024-03-07 23:02:24 +0100

    Modification of the .py file

--- GCP_PLN.py
+++ GCP_PLN.py
@@ -1,45 +1,17 @@
-from datetime import date
+import requests
 import pandas as pd
-import requests 
+from bs4 import BeautifulSoup
 
-# import csv
+url = "https://nbp.pl/statystyka-i-sprawozdawczosc/kursy/cena-zlota/"
+data = requests.get(url).text
 
-def print_d2k(file_path, mnemonic, datee, value):
-    
-    f = open(file_path,"w+")
+soup = BeautifulSoup(data, 'html.parser')
 
-    f.write("TSERIES_START;\n")
-    f.write("    CHANGE;\n")
-    f.write("    IDENT_START;\n")
-    f.write("        DRI_MNEMONIC:\"" + mnemonic + "\";\n")
-    f.write("        SERIES_TYPE:\"HIST\";\n")
-    f.write("    IDENT_END;\n")
-    f.write("    VALUES:\"" + str(datee.strftime("%Y.%m.%d")) + "\" = " + str(value) + ";\n")
-    f.write("TSERIES_END;\n")
+print('Classes of each table:')
+for table in soup.find_all('table'):
+    print(table.get('class'))
 
-    f.close()
+tables = soup.find_all('table')
 
-rates = "https://www.boz.zm/AVERAGE_FXRATES.xlsx"
-response = requests.get(rates)
+table = soup.find('table', class_='table table-hover')
 
-xls = open('ZMB_EXR.xls', 'wb')
-xls.write(response.content)
-xls = pd.ExcelFile(r"ZMB_EXR.xls")
-sheet = xls.parse(0)
-# print(type(sheetX.iloc[-1][1]))
-# print(sheet.iloc[-1][2])
-# print(sheet.iloc[-1][3])
-xls.close()
-
-source_date = sheet.iloc[-1][1].date()
-# print(source_date)
-
-today = date.today()
-# print(today)
-
-if source_date == today:  # checking if date in the source is equal to current day
-    buy = sheet.iloc[-1][2]
-    sale = sheet.iloc[-1][3]
-    print_d2k('outputB.d2k', 'ZMBUSDZMWSPB.F', source_date, buy)
-    print_d2k('outputA.d2k', 'ZMBUSDZMWSPA.F', source_date, sale)
-else: pass
